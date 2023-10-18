052ffaa5e38f7b9f5b9efe47d2a5395b24032493
chris <chris.gifford321@gmail.com>
2023-10-18 14:25:00 +0100

    First challenge DNA-Pairs completed

--- dna-pairs/__tests__/dna-pairs.test.js
+++ dna-pairs/__tests__/dna-pairs.test.js
@@ -1,7 +1,26 @@
-const dnaPairs = require('../dna-pairs.js');
+const dnaPair = require('../dna-pairs.js');
 
-describe('dnaPairs()', () => {
-  test('ADD YOUR TEST DESCRIPTION HERE', () => {
-    // add your test assertion(s) here
-  });
+test('handles empty string input', () => {
+  expect(dnaPair("")).toEqual([]);
+});
+
+test('handles lowercase input', () => {
+  expect(dnaPair("atcg")).toEqual([["A", "T"], ["T", "A"], ["C", "G"], ["G", "C"]]);
+});
+
+test('pairs single base G correctly', () => {
+  expect(dnaPair("G")).toEqual([["G", "C"]]);
+});
+
+test('pairs AG correctly', () => {
+  expect(dnaPair("AG")).toEqual([["A", "T"], ["G", "C"]]);
+});
+
+test('pairs ATAG correctly', () => {
+  expect(dnaPair("ATAG")).toEqual([["A", "T"], ["T", "A"], ["A", "T"], ["G", "C"]]);
+});
+
+
+test('ignores invalid letters in ATZG', () => {
+  expect(dnaPair("ATZG")).toEqual([["A", "T"], ["T", "A"], ["G", "C"]]);
 });
--- dna-pairs/dna-pairs.js
+++ dna-pairs/dna-pairs.js
@@ -1,6 +1,27 @@
 // Please do not change the name of this function
 function dnaPairs(dna) {
-  // Write your code here
+ 
+    let result = [];
+    
+    let upperDna = dna.toUpperCase();
+    
+    for (let i = 0; i < upperDna.length; i++) {
+      let letter = upperDna[i];
+        
+      if (letter === 'A') {
+            result.push(['A', 'T']);
+        } else if (letter === 'T') {
+            result.push(['T', 'A']);
+        } else if (letter === 'C') {
+            result.push(['C', 'G']);
+        } else if (letter === 'G') {
+            result.push(['G', 'C']);
+        } 
+    }
+    
+    return result;
+
+
 }
 
 module.exports = dnaPairs;
