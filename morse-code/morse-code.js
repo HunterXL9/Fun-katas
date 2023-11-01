7b9a1b3b76d5048043534c5a3c43071669776e23
chris <chris.gifford321@gmail.com>
2023-11-01 16:43:08 +0000

    working with w now was lowercase in the data file

--- morse-code/morse-code.js
+++ morse-code/morse-code.js
@@ -13,15 +13,26 @@ function morseCode (input,toMorse = false) {
 function morseToTextDecode(input){
     return input.split("   ")
     .map(word => word.split(" ") 
-    .map(character => morseToText[character])
+    .map(character => {
+        const textCharacter = morseToText[character];
+        // console.log(textCharacter); 
+        return textCharacter;
+      })
     .join("")).join(" ");
-}
+    }
 function textToMorseEncode(textStr){
     const upperText = textStr.toUpperCase();
     return upperText.split(" ")
     .map(word => word.split("")
     .map(character => textToMorse[character])
-    .join(" ")).join("   "); 
+    .join(" ")
+    )
+    .join("   "); 
 };
 
 module.exports = morseCode;
+
+const morseText = morseCode('Hello World', true);
+console.log(morseText);
+const decodedText = morseCode('.... . .-.. .-.. ---   .-- --- .-. .-.. -..', false);
+console.log(decodedText);
